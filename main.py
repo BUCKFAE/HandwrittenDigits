fc9bdc3ada3e266425b4811fe37cd42749892fb0
Julian Schubert <informatik.schubert@gmail.com>
2021-08-05 19:56:24 +0200

    XOR working, testing MNIST

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import numpy as np
 import sys
 
 LEARNING_RATE = 0.05
-TRAINING_SIZE = 4000   # Zero for entire MNIST Dataset
-ARCHITECTURE = [784, 32, 16, 10]
-ITERATIONS = 500
-BATCH_SIZE = 128
+TRAINING_SIZE = 0   # Zero for entire MNIST Dataset
+ARCHITECTURE = [784, 16, 16, 10]
+BATCH_SIZE = 16
 
 def to_one_hot(x):
     """Returns the one hot encoding of the given x"""
@@ -55,89 +54,103 @@ def bpropagate(W, x):
 # Storing loss for all generations for plotting
 loss_hist = []
 
-# Training the model
-for iteration in range(ITERATIONS):
+def get_loss(t, y):
+    assert len(t) == len(y)
+    return sum([(t[d][0] - y[d][0]) ** 2 for d in range(len(t))])
+    
+def sig(x):
+    return 1 / (1 + np.exp(-x))
 
-    # Storing stats for this gen
+def sigg(x):
+    return sig(x) * (1 - sig(x))
+
+def propagate(x, w):
+
+    # Adding bias neuron
+    x = np.insert(x, 0, 1, axis=0)
+
+    net = w.dot(x)
+    return sig(net)
+
+print(f"Netowrk: " + " -> ".join([str(a) for a in ARCHITECTURE]))
+
+weight_matrices = [np.random.sample(ARCHITECTURE[l] * (ARCHITECTURE[l - 1] + 1)).reshape((ARCHITECTURE[l], ARCHITECTURE[l - 1] + 1)) for l in range(1, len(ARCHITECTURE))]
+print(f"Weights: " + " -> ".join([f"{w.shape}" for w in weight_matrices]))
+
+iteration = 0
+
+while True:
+
+    iteration += 1
+
     total_loss = 0
     correct = 0
-    
-    # Storing gradients for weights
-    gradients = [np.zeros_like(w, dtype='float64') for w in weights]
+
+    gradients = [np.zeros_like(w, dtype='float64') for w in weight_matrices]
+
+    data_point_id = 0
 
     # Iterating over all datapoints in the training data
     for d in range(len(x_train)):
 
+        data_point_id += 1
+
         # Input and target outpug
-        xd, td = np.array(x_train[d], dtype='float64'), y_train[d]
+        x, t = np.array(x_train[d], dtype='float64'), y_train[d]
 
-        # Stores network output for all layer
-        od = [xd]
-        
-        # Calculates network output
-        for w in weights:
-            res = propagate(w, od[-1])
-            od.append(res)
+        od = [x]
+        for weight_matrix in weight_matrices:
+            od.append(propagate(od[-1], weight_matrix))
 
-        # Updating loss / accuracy for given data point
-        loss = sum([(od[-1][i][0] - td[i][0]) ** 2 for i in range(ARCHITECTURE[-1])])
+        correct += 1 if np.argmax(od[-1]) == np.argmax(t) else 0
+
+        loss = get_loss(t, od[-1])
         total_loss += loss
 
-        # Counter for MNIST data
-        correct += 1 if np.argmax(od[-1]) == np.argmax(td) else 0
-        
-        # Use this when using XOR data
-        # correct += 1 if abs(od[-1] - td) < 0.5 else 0
+        d = []
 
-        # Stores delta for neurons of prev layer
-        delta_prev = None
+        # Backpropagation
+        for l in range(len(weight_matrices) - 1, -1, -1):
+            #print(f"Current layer: {l}")
+            if l == len(weight_matrices) - 1: 
+                
+                d.insert(0, (od[-1] - t) * od[-1] * (1 - od[-1]))
+                grad = d[0] @ np.transpose(np.insert(od[-2], 0, 1, axis=0))
 
-        # Updating weight matrices between all layer
-        for l in range(len(weights) - 1, -1, -1):
-         
-            # Updating last layer
-            if l == len(weights) - 1:  
+                assert gradients[l].shape == grad.shape
+                gradients[l] += grad            
+            
+            else: # Last layer
 
-                # If using sigmoid: delta_prev = (od - td) * od * (1 - od)
-                delta_prev = (od[-1] - td) * od[-1] * (1 - od[-1])
+                oj = od[l + 1]
+                dl = d[0]
+                w = weight_matrices[l + 1]
+                
+                # Weights x delta prev layer
+                wd = np.transpose(w) @ dl
 
-                # Grad weights: delta x output last layer, inserting 1 for bias
-                grad = delta_prev @ np.transpose(np.insert(od[-2], 0, 1, axis=0))
-  
-                # Appending gradient
+                # Removing delta for bias neuron
+                wd = np.delete(wd, 0, axis=0)
+
+                # d = sum (wjl dl) * oj * (1 - oj)                
+                d.insert(0, wd * oj * (1 - oj))
+
+                # oi with bias neuron
+                p2 = np.transpose(np.insert(od[l], 0, 1, axis=0))
+
+                # grad = d * p2               
+                grad = d[0] * p2
+
+                assert grad.shape == weight_matrices[l].shape
                 assert grad.shape == gradients[l].shape
                 gradients[l] += grad
-
-            else:
                 
-                # Getting the neurons of the prev layer
-                weights_prev = np.transpose(weights[l + 1])
-
-                # Delta current layer is f'(W dot delta_prev)
-                delta_curr = bpropagate(weights_prev, delta_prev)
-
-                # Deleting delta for bias neuron
-                delta_curr = np.delete(delta_curr, 0, axis=0)
-
-                # Ouptut of neurons current layer (with added bias neuron)
-                activations_curr = np.transpose(np.insert(od[l], 0, 1, axis=0))
-
-                # Delta weights: delta neurons x output current layer
-                delta_weights = delta_curr.dot(activations_curr)
-
-                # Adding the gradients of the weights
-                assert delta_weights.shape == gradients[l].shape
-                gradients[l] += delta_weights
-
-                # Storing current delta of neurons for next layer
-                delta_prev = delta_curr
-
         # Updating weights at end of batch
-        if d == len(x_train) - 1 or d % BATCH_SIZE == 0:
+        if data_point_id % BATCH_SIZE == 0:
             # Updating weights of all layer
-            for l in range(len(weights)):
-                weights[l] -= LEARNING_RATE * (1.0 / BATCH_SIZE) * gradients[l]
-            gradients = [np.zeros_like(w) for w in weights]
+            for l in range(len(weight_matrices)):
+                weight_matrices[l] -= LEARNING_RATE * (1.0 / BATCH_SIZE) * gradients[l]
+            gradients = [np.zeros_like(w) for w in weight_matrices]
 
     # Updating loss
     total_loss *= (1.0 / len(x_train))
@@ -148,7 +161,3 @@ for iteration in range(ITERATIONS):
     print(f"\t\tmax weight: {max([np.max(a) for a in weights]):9.5f}", end="")
     print(f"\tmin weight: {min([np.min(a) for a in weights]):9.5f}", end="")
     print(f"\tavg weight: {sum([np.sum(a) for a in weights]) / sum([a.shape[0] * a.shape[1] for a in weights]):9.5f}")
-
-# Plotting loss
-# plt.plot([i for i in range(ITERATIONS)], loss_hist)
-# plt.show()
